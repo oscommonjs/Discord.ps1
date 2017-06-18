e52ef8f99d060472e141cdbfee49f815dbff839c
Brianna Foxwell <bumblebrie@cock.li>
2017-06-18 18:11:41 +0100

    Added text to auth example

--- Public/Invoke-DiscordBot-Dev.ps1
+++ Public/Invoke-DiscordBot-Dev.ps1
@@ -11,3 +11,6 @@ Function Invoke-DiscordBot {
     Set-PSDiscordConfig -Path $PSDiscordConfigPath -token $Token
     $DiscordSession = Invoke-RestMethod -Uri https://discordapp.com/api/oauth2/token -Body @{token="$Token"}
     Write-Log "Logged in as $($DiscordSession.self.name)" -Path $LogPath
+
+# THIS IS VERY INCOMPLETE, THE REST OF THE FUNCTION IS CURRENTLY UNDERGOING TESTING
+# CURRENTLY THIS IS HERE FOR A EXAMPLE OF HOW TO AUTH TO THE SOCKETS
